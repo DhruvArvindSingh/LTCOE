465d49fafe2a6c56ad570f7a2012d1c3824c5e67
DhruvArvindSingh <dhruvsinghxd@gmail.com>
2025-01-29 16:00:24 +0530

    Added Signin Signup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import { useState } from 'react'; 
-import Job_main from './pages/Job_main'; 
-import JobDetailsPage from './pages/JobDetailsPage'; 
-import Chatbot from './pages/Chatbot'; 
-import Navbar from './components/Navbar'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Job_main from './pages/Job_main';
+import JobDetailsPage from './pages/JobDetailsPage';
+import Chatbot from './pages/Chatbot';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Navbar from './components/Navbar';
 import Dashboard from "./dashboard/index.jsx";
 import ViewResume from "./my-resume/view/index.jsx";
 import EditResume from "./dashboard/resume/[resumeId]/edit/index.jsx";
@@ -12,28 +14,30 @@ import './App.css'
 
 
 function App() {
-  const [searchParams, setSearchParams] = useState({ jobTitle: 'developer', location: '', datePosted: 'all' }); 
-  const handleSearch = (params, navigate) => { 
-    setSearchParams(params); 
-    navigate('/jobs'); 
+  const [searchParams, setSearchParams] = useState({ jobTitle: 'developer', location: '', datePosted: 'all' });
+  const handleSearch = (params, navigate) => {
+    setSearchParams(params);
+    navigate('/jobs');
   };
 
   return (
     <>
-      <Router> 
-      <Navbar onSearch={handleSearch} /> 
-      <Routes> 
-        <Route path="/" element={<h1>Home Page</h1>} /> 
-        {/* <Route path="/chatbot" element={<Chatbot />} />  */}
-        <Route path="/job" element={<Job_main searchParams={searchParams} />} /> 
-        <Route path="/job/:id" element={<JobDetailsPage />} /> 
-        <Route path="/resume" element={<Dashboard />} /> 
-        <Route path="/resume/edit" element={<EditResume />} /> 
-        <Route path="/resume/:email/:resumeId/view" element={<ViewResume />} /> 
-        <Route path="/chatbot" element={<Chatbot />} /> 
-      </Routes>
+      <Router>
+        {/* <Navbar onSearch={handleSearch} />  */}
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          {/* <Route path="/chatbot" element={<Chatbot />} />  */}
+          <Route path="/job" element={<Job_main searchParams={searchParams} />} />
+          <Route path="/job/:id" element={<JobDetailsPage />} />
+          <Route path="/resume" element={<Dashboard />} />
+          <Route path="/resume/edit" element={<EditResume />} />
+          <Route path="/resume/:email/:resumeId/view" element={<ViewResume />} />
+          <Route path="/chatbot" element={<Chatbot />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
 
-    </Router>
+      </Router>
     </>
   )
 }
